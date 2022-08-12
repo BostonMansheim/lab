3c9fb99d661de391471c5800b634f3b15c65bcbd
BostonMansheim <106132026+BostonMansheim@users.noreply.github.com>
2022-08-12 01:07:07 -0500

    Update test_classes.py
    added more rigorous testing

--- test_classes.py
+++ test_classes.py
@@ -15,24 +15,52 @@ class MyTestCase(unittest.TestCase):
     def test_power(self):
         self.tv1.power()
         assert self.tv1.__str__()[19:23] == 'True'  # add assertion here
+        self.tv1.power()
+        assert self.tv1.__str__()[19:24] == 'False'  # add assertion here
 
     def test_channel_up(self):
+        self.tv1.channel_up()
+        assert self.tv1.__str__()[36] == '0'  # add assertion here
+
         self.tv1.power()
         self.tv1.channel_up()
         assert self.tv1.__str__()[35] == '1'  # add assertion here
+        self.tv1.channel_up()
+        assert self.tv1.__str__()[35] == '2'  # add assertion here
+        self.tv1.channel_up()
+        assert self.tv1.__str__()[35] == '3'  # add assertion here
+        self.tv1.channel_up()
+        assert self.tv1.__str__()[35] == '0'  # add assertion here
 
     def test_channel_down(self):
+        self.tv1.channel_down()
+        assert self.tv1.__str__()[36] == '0'  # add assertion here
+
         self.tv1.power()
         self.tv1.channel_down()
         assert self.tv1.__str__()[35] == '3'  # add assertion here
+        self.tv1.channel_down()
+        assert self.tv1.__str__()[35] == '2'  # add assertion here
 
     def test_volume_up(self):
+        self.tv1.volume_up()
+        assert self.tv1.__str__()[48] == '0'  # add assertion here
+
         self.tv1.power()
         self.tv1.volume_up()
         assert self.tv1.__str__()[47] == '1'  # add assertion here
+        self.tv1.volume_up()
+        assert self.tv1.__str__()[47] == '2'  # add assertion here
+        self.tv1.volume_up()
+        assert self.tv1.__str__()[47] == '2'  # add assertion here
 
     def test_volume_down(self):
+        self.tv1.volume_down()
+        assert self.tv1.__str__()[48] == '0'  # add assertion here
+
         self.tv1.power()
+        self.tv1.volume_down()
+        assert self.tv1.__str__()[47] == '0'  # add assertion here
         self.tv1.volume_down()
         assert self.tv1.__str__()[47] == '0'  # add assertion here
 
